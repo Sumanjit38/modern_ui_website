b9805283763551d8887ca56147c31c80e27a62a2
Sumanjit38 <sumanjit38@gmail.com>
2023-04-25 13:28:16 +0530

    create app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+//import Article from './components/article/Article'
+//import Brand from './components/brand/Brand'
+//import CTA from './components/cta/CTA'
+//import Feature from './components/feature/Feature'
+//import Navbar from './components/navbar/Navbar'
+import { Article, Brand, CTA, Feature } from './components';
 
 const App = () => {
   return (
