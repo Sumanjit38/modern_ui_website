6fe38d525de5c3349cca007e847257829cd5f31b
Sumanjit38 <sumanjit38@gmail.com>
2023-04-25 13:41:11 +0530

    create containers

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+ <div>Blog</div>
+  )
+}
+export default Header;
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatGPT3.css'
+
+const WhatGPT3 = () => {
+  return (
+ <div>Blog</div>
+  )
+}
+export default WhatGPT3;
